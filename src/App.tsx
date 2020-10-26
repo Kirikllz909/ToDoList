6b1392ee2a13283130dae8a16dd7a1504d2b0f4c
Kirikllz909 <kirikllz909@gmail.com>
2020-10-26 18:59:26 +0300

    Build main template to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="h1 textAlignCenter">ToDo List</h1>
+      <div className="divBorder">
+        <h5 className="h5 ml-2 mt-2">Enter the action to add to the list:</h5>
+        <div className="parentFlex">
+          <input type="text" className="form-control" />
+          <button className="btn btn-primary inlineButton ml-2">ADD</button>
+        </div>
+      </div>
     </div>
   );
 }
