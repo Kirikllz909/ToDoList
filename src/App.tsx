42ade9062f7223d42ab7d4e4ee4d9055dbbf0ca6
Kirikllz909 <kirikllz909@gmail.com>
2020-10-27 17:53:19 +0300

    move adding todos to ToDoList

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,6 @@ const App: React.FC = () => {
       <h1 className="h1 textAlignCenter">ToDo List</h1>
       <div className="divBorder">
         <h5 className="h5 ml-2 mt-2">Enter the action to add to the list:</h5>
-        <div className="parentFlex">
-          <input type="text" className="form-control" />
-          <button className="btn btn-primary inlineButton ml-2">ADD</button>
-        </div>
         <ToDoList />
       </div>
     </div>
