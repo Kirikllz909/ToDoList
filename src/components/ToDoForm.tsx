4fae530f985fbaaac0808308a9189eccf287386a
Kirikllz909 <kirikllz909@gmail.com>
2020-10-27 21:04:06 +0300

    Created form for adding todo elements

---
+++ src/components/ToDoForm.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+export const ToDoForm: React.FC<any> = (props) => {
+  const [textValue, setTextValue] = useState("");
+  let inputRef: React.RefObject<HTMLInputElement>;
+  inputRef = React.createRef();
+
+  //When "Enter" key is pressed in input element you add this action to todo list
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      props.onSubmit(textValue);
+      inputRef.current!.value = "";
+    }
+  };
+
+  //Handle change of input element
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTextValue(e.target.value);
+  };
+
+  return (
+    <div className="parentFlex">
+      <input
+        onChange={handleChange}
+        placeholder="Enter the action here"
+        type="text"
+        ref={inputRef}
+        className="form-control"
+        onKeyPress={handleKeyPress}
+      />
+      <button
+        type="button"
+        onClick={() => {
+          props.onSubmit(textValue);
+          inputRef.current!.value = "";
+        }}
+        className="btn btn-primary inlineButton ml-2"
+      >
+        ADD
+      </button>
+    </div>
+  );
+};
